0e9cfe4c8272f456c73fd4143b42263c0ee12571
diaseu <diana.seung@gmail.com>
2024-10-03 23:02:12 -0700

    resolve netlify

--- src/component/Bento.js
+++ src/component/Bento.js
@@ -1,12 +1,4 @@
 import React from "react";
-import { CheckIcon } from "@heroicons/react/20/solid";
-
-const includedFeatures = [
-  "Private forum access",
-  "Member resources",
-  "Entry to annual conference",
-  "Official member t-shirt",
-];
 
 export default function Bento() {
   return (
